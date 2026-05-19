57faa2f796207a79118fa45f0f4486b79c68e0bf
Hau Tran Nhan <hautn3@fpt.com>
2026-05-19 13:26:10 +0900

    feat: setup prisma service and global validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
   const port = process.env.PORT || 3001;
   await app.listen(port);
